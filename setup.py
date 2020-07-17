1db3f04d09bc63aaea09dea9d0d7936e2336d321
Celray James <celray.chawanda@outlook.com>
2020-07-17 19:02:26 +0200

    renamed package to avoid confusion with original package

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@
 
 from setuptools import setup
 
-setup(name='pystran',
-      version='0.1',
+setup(name='pystran3',
+      version='0.3',
       description='Set of diagnostic tools for model structure analysis',
       url='https://github.com/stijnvanhoey/pystran',
       author='Stijn Van Hoey',
       author_email='stijnvanhoey@gmail.com',
-      packages=['pystran'],
+      packages=['pystran3'],
       license='BSD 3-clause New or Revised License',
       install_requires=['matplotlib', 'numpy', 'scipy'],
       keywords='modelling, sensitivity analysis, optimization, metric calculation',)
